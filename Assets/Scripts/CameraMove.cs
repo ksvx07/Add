a1061edd7ae086104c1fff4b08fe86f36d1ac9e5
nut4985 <50408675+nut4985@users.noreply.github.com>
2025-09-03 22:25:48 +0900

    Update CameraMove.cs

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -12,6 +12,8 @@ public class CameraMove : SingletonObject<CameraMove>
     private const float minPitch = -40f;
     private const float maxPitch = 10f;
 
+    Vector3 desiredPosition;
+
     void Start()
     {
         yaw = transform.eulerAngles.y;
@@ -32,8 +34,17 @@ public class CameraMove : SingletonObject<CameraMove>
         pitch -= mouseY;
 
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        desiredPosition = target.position + Quaternion.Euler(pitch, yaw, 0) * offset;
 
-        transform.position = target.position + Quaternion.Euler(pitch, yaw, 0) * offset;
+        Ray ray = new Ray(target.position, desiredPosition - target.position);
+        Debug.DrawLine(target.position, desiredPosition, Color.red);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo,(target.position - desiredPosition).magnitude))
+        {
+            desiredPosition = hitInfo.point;
+        }
+
+        transform.position = desiredPosition;//target.position + Quaternion.Euler(pitch, yaw, 0) * offset;
         transform.LookAt(target.position + Vector3.up * 1.5f);
     }
 }

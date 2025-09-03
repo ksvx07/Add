67fd27fac158da4cbe57b20af96fc09d4db4fe93
이종은 <94448168+ksvx07@users.noreply.github.com>
2025-09-04 00:15:22 +0900

    update main

---
+++ Assets/Scripts/TeleportToNextLevel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TeleportToNextLevel : MonoBehaviour
+{
+    public Transform tpExit;
+    public Transform tpEnter;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(GameConstant.playerTag))
+        {
+            other.transform.position = other.transform.position + tpEnter.position - tpExit.position;
+        }
+    }
+}
